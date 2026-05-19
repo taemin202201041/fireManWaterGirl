bcef773525a65c5b59a58fce27b42e0da1374982
dojungke <doridori2363@gmail.com>
2026-05-19 20:18:59 +0900

    레이어 기반으로 충돌처리 개편 및 젬 추가
    Water와 Fire는 서로 같은 레이어일 경우에만 충돌 처리를 함 젬 추가 현제 부숴지는 기능만 있고 게임메니져 추가후 주석 부분 적용 필요

---
+++ Assets/_Project/Scripts/Environment/Gem.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 충돌한 대상이 플레이어인지 확인
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log(collision.name + " 젬 획득!");
+            //게임메니저에서 젬획득 카운트
+            // GameManager.Instance.gemCount += 1;
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
